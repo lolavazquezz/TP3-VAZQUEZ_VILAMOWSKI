e13b9fe860e73a4ab48c634da6846c0d595fd430
Lola Vazquez <127701031+lolavazquezz@users.noreply.github.com>
2024-04-16 08:08:07 -0300

    Add files via upload

--- TP3-VAZQUEZ_VILAMOWSKI/src/index.js
+++ TP3-VAZQUEZ_VILAMOWSKI/src/index.js
@@ -87,9 +87,51 @@ app.get('/wrapper/omdbgetbyimdbid', async (req, res) => {
     res.send(resultado)
     res.status(200).send("OK")
 })
-//
-// Inicio el Server y lo pongo a escuchar.
-//
+
+const alumnosArray = [];
+alumnosArray.push(new Alumno("Esteban Dido" , "22888444", 20));
+alumnosArray.push(new Alumno("Matias Queroso", "28946255", 51));
+alumnosArray.push(new Alumno("Elba Calao" , "32623391", 18));
+app.get('/alumno', (req, res) => {
+    let resultado = alumnosArray.toString();
+    res.send(resultado)
+    res.status(200).send("OK")
+})
+
+app.get('/alumno/:dni', (req, res) => {
+    const dni = req.params;
+    const alumnoEncontrado = alumnos.find(alumno => alumno.dni === dni);
+    if (!alumnoEncontrado) {
+    res.status(404).send("Alumno no encontrado");
+    }
+    res.status(200).send(alumnoEncontrado);
+})
+
+app.post('/alumno', (req, res) => {
+    const { username, dni, edad } = req.body;
+    if (!username || !dni || !edad) {
+    res.status(400).send("Faltan campos");
+    }
+
+    const nuevoAlumno = new Alumno(username, dni, edad);
+    alumnos.push(nuevoAlumno);
+
+    res.status(201).send("Alumno creado satisfactoriamente");
+})
+
+app.delete('/alumno', (req, res) => {
+    const dni = req.body;
+    if (!dni) {
+    res.status(400).send("Faltan el campo dni");
+    }
+    const alumnoIndex = alumnos.findIndex(alumno => alumno.dni === dni);
+    if (alumnoIndex === -1) {
+        return res.status(404).send("Alumno no encontrado");
+    }
+    alumnos.splice(alumnoIndex, 1);
+    res.status(200).send("Alumno eliminado correctamente");
+})
+
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
