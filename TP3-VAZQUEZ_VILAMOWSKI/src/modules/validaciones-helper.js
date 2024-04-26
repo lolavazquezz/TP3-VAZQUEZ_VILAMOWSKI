30b69882eb86b3ca09e9c10f85d581d9b09f41a1
Lola Vazquez <127701031+lolavazquezz@users.noreply.github.com>
2024-04-26 11:00:53 -0300

    Update validaciones-helper.js

--- TP3-VAZQUEZ_VILAMOWSKI/src/modules/validaciones-helper.js
+++ TP3-VAZQUEZ_VILAMOWSKI/src/modules/validaciones-helper.js
@@ -1,11 +1,11 @@
 class ValidacionesHelper {
         getIntegerOrDefault = (value, defaultValue) => {
-            if (isNaN(parseInt(value) || value == null)) return defaultValue;
-            return parseInt(value);
-    };
-        getStringOrDefault = (value, defaultValue) => {
-            if (!isNaN(value) || value == null) return defaultValue;
-            return String(value);
-    };
+
+        if (typeof value !== 'number') return defaultValue;
+        return value;
+    }
+    getStringOrDefault = (value, defaultValue) => {
+        if (typeof value !== 'string') return defaultValue;
+        return value;
     }
     export default new ValidacionesHelper();
